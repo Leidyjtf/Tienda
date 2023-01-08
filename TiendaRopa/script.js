f72b7aba4238c88c19dbc5957c34585981f77119
Leidyjtf <87446478+Leidyjtf@users.noreply.github.com>
2023-01-08 17:53:12 -0500

    Add files via upload

---
+++ TiendaRopa/script.js
@@ -0,0 +1,36 @@
+//LOS MAS VENDIDOS
+//----------------
+
+function App(){}
+
+window.onload = function(event){
+    var app = new App();
+    window.app = app;
+}
+
+App.prototype.processingButton = function(event){
+
+    const btn = event.currentTarget;
+    const carruselList = event.currentTarget.parentNode;
+    const track = event.currentTarget.parentNode.querySelector('#track');
+    const carrusel = track.querySelectorAll('.masvenCarrusel');
+
+    const carruselWidth = carrusel[0].offsetWidth;
+    const trackWidth = track.offsetWidth;
+    const listWithd = carruselList.offsetWidth;
+
+    track.style.left == "" ? leftPosition = track.style.left = 0 : leftPosition = parseFloat(track.style.left.slice(0, -2) * -1);
+    btn.dataset.button == "button-prev" ? prevAction(leftPosition, carruselWidth, track) : nextAction(leftPosition, trackWidth, listWithd, carruselWidth, track);
+}
+
+let prevAction = (leftPosition , carruselWidth, track) => {
+    if(leftPosition > 0){
+        track.style.left = `${-1 * (leftPosition - carruselWidth)}px`;
+    }
+}
+
+let nextAction = (leftPosition, trackWidth, listWithd, carruselWidht, track) => {
+    if (leftPosition < (trackWidth - listWithd)) {
+        track.style.left = `${-1 * (leftPosition + carruselWidht)}px`;
+    }
+}
